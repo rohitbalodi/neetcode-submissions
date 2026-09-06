e1228f88f2b90b2756c44605574ce9195c5b7709
rohitbalodi <rohit.balodi@gmail.com>
2026-09-06 21:02:53 +0530

    Add: jump-game-ii - submission-1

---
+++ Data Structures & Algorithms/jump-game-ii/submission-1.go
@@ -0,0 +1,19 @@
+func jump(nums []int) int {
+	if len(nums)==1 {
+		return 0
+	}
+
+	dp := make([]int, len(nums)+1)
+	dp[0] = 0
+	for i, num := range nums {
+		for j:=i+1; j<i+1+num && j<len(dp); j++ {
+			if dp[j]==0 {
+				dp[j] = dp[i]+1
+			} else {
+				dp[j] = min(dp[j], dp[i]+1)
+			}
+		}
+	}
+
+	return dp[len(dp)-2]
+}
