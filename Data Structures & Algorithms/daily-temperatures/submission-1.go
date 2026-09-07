bc2a5ddb770fcafea3e993893806bf2055e53e86
rohitbalodi <rohit.balodi@gmail.com>
2026-09-07 12:18:06 +0530

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.go
@@ -0,0 +1,15 @@
+func dailyTemperatures(temperatures []int) []int {
+	stack := [][]int{{101,-1}}
+	output := make([]int, len(temperatures))
+
+	for i:=0; i<len(temperatures); i++ {
+		for stack[len(stack)-1][0] < temperatures[i] {
+			index := stack[len(stack)-1][1]
+			output[index] = i-index
+			stack = stack[:len(stack)-1]
+		}
+		stack = append(stack, []int{temperatures[i], i})
+	}
+
+	return output
+}
