1d8a8802e5333ca10845346a668ad9904c21e6b3
rohitbalodi <rohit.balodi@gmail.com>
2026-09-08 18:18:04 +0530

    Add: meeting-schedule - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule/submission-0.go
@@ -0,0 +1,21 @@
+/**
+ * Definition of Interval:
+ * type Interval struct {
+ *    start int
+ *    end   int
+ * }
+ */
+
+func canAttendMeetings(intervals []Interval) bool {
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i].end < intervals[j].end
+	})
+
+	for i:=0; i<len(intervals)-1; i++ {
+		if intervals[i].end > intervals[i+1].start {
+			return false
+		}
+	}
+	return true
+
+}
