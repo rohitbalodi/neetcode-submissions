280775e591190ecc0a44bcd131058d7f79e62006
rohitbalodi <rohit.balodi@gmail.com>
2026-09-08 18:08:13 +0530

    Add: non-overlapping-intervals - submission-1

---
+++ Data Structures & Algorithms/non-overlapping-intervals/submission-1.go
@@ -0,0 +1,23 @@
+func eraseOverlapIntervals(intervals [][]int) int {
+    sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][0] < intervals[j][0]
+	})
+
+	stack :=[][]int{{intervals[0][0], intervals[0][1]}}
+
+	output := 0
+	for i:=1; i<len(intervals); i++ {
+		top := stack[len(stack)-1]
+		if intervals[i][0]<top[1] {
+			output++
+			if top[1]>intervals[i][1] {
+				top[0] = intervals[i][0]
+				top[1] = intervals[i][1]
+			}
+		} else {
+			stack = append(stack, intervals[i])
+		}
+	}
+
+	return output
+}
