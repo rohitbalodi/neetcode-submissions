ad56416bbad3bd7a726ead844f3b62b49cac4bed
rohitbalodi <rohit.balodi@gmail.com>
2026-09-10 14:42:32 +0530

    Add: populating-next-right-pointers-in-each-node - submission-1

---
+++ Data Structures & Algorithms/populating-next-right-pointers-in-each-node/submission-1.go
@@ -0,0 +1,42 @@
+/**
+ * Definition for a Node.
+ * type Node struct {
+ *     Val int
+ *     Left *Node
+ *     Right *Node
+ *     Next *Node
+ * }
+ */
+
+func connect(root *Node) *Node {
+	if root == nil {
+		return root
+	}
+	curr := root
+	queue := []*Node{ curr, nil}
+	for len(queue)>0 {
+		front := queue[0]
+		queue = queue[1:]
+		// since its a nil, need not set next here
+		if front == nil {
+			// last nil
+			if len(queue)==0 {
+				return root
+			}
+			// maybe change of level
+			queue = append(queue, nil)
+		} else {
+			// so let's check if these are full nodes
+			if front.Left != nil && front.Right != nil {
+				queue = append(queue, front.Left)
+				queue = append(queue, front.Right)
+			}
+			// else do nothing since they are leaf nodes
+			front.Next = queue[0]
+		}
+
+	}
+
+	return root
+	
+}
